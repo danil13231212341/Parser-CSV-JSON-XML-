03399791cd34be566559c74824afddcee4ac6bdb
Devonty <Devonty@yandex.ru>
2023-10-21 20:40:06 +0300

    SemiWork json

--- src/ru/vsu/sc/parser/JSONParser.java
+++ src/ru/vsu/sc/parser/JSONParser.java
@@ -3,13 +3,54 @@ package ru.vsu.sc.parser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.Key;
 import java.util.*;
 
 public class JSONParser {
+    private static class IndexWapper {
+        private int index = 0;
+        private final String data;
 
-    public static Map<String, Object> parseJSON(String filePath) {
-        String jsonData = readFile(filePath);
-        return parseJSONObject(jsonData);
+        public IndexWapper(String data) {
+            this.data = data.trim();
+        }
+
+        public Character charNow() {
+            return data.charAt(index);
+        }
+
+        public void next() {
+            index++;
+        }
+
+        public boolean hasNext() {
+            return index + 1 < data.length();
+        }
+
+        private void skipSpaces() {
+            while (Character.isWhitespace(data.charAt(index))) next();
+        }
+
+        private int nextWhileNot(Character chr) {
+            /*
+            stays if now on chr.
+             */
+            while (!Objects.equals(charNow(), chr)) next();
+            return index;
+        }
+
+        public Character nextWhileNotIn(String str) {
+            while ( !str.contains(String.valueOf(charNow()))) next();
+            return charNow();
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public String getData() {
+            return data;
+        }
     }
 
     private static String readFile(String filePath) {
@@ -25,39 +66,83 @@ public class JSONParser {
         return content.toString();
     }
 
-    @SuppressWarnings("unchecked")
-    private static Map<String, Object> parseJSONObject(String jsonData) {
+    public static Object parseJSON(String filePath) {
+        String jsonData = readFile(filePath);
+        return parseJSONbyData(jsonData);
+    }
+
+    private static Object parseJSONbyData(String jsonData) {
+        return parseValue(new IndexWapper(jsonData));
+    }
+
+    private static Map<String, Object> parseMap(IndexWapper iw) {
+        String jsonData = iw.getData();
         Map<String, Object> jsonMap = new LinkedHashMap<>();
 
-        jsonData = jsonData.trim();
-        if (jsonData.startsWith("{") && jsonData.endsWith("}")) {
-            jsonData = jsonData.substring(1, jsonData.length() - 1);
+        // value
+        int indexSave;
 
-            String[] keyValuePairs = jsonData.split(",");
-            for (String keyValuePair : keyValuePairs) {
-                String[] keyValue = keyValuePair.split(":", 2);
-                String key = keyValue[0].trim().replace("\"", "");
-                Object value = parseValue(keyValue[1]);
-                jsonMap.put(key, value);
-            }
-        } else {
-            throw new IllegalArgumentException("Invalid JSON format");
+        iw.next();
+        iw.skipSpaces();
+        while (iw.charNow() != '}') {
+            Character charNow = iw.charNow();
+            // Key Search
+            indexSave = iw.nextWhileNot('"');
+            iw.next();
+            iw.nextWhileNot('"');
+            String key = jsonData.substring(indexSave + 1, iw.getIndex());
+            // Value
+            iw.nextWhileNot(':');
+            iw.next();
+            iw.skipSpaces();
+            Object value = parseValue(iw);
+            // Adding
+            jsonMap.put(key, value);
+            iw.skipSpaces();
         }
-
+        iw.next();
         return jsonMap;
     }
 
-    private static Object parseValue(String value) {
-        if (value.startsWith("\"") && value.endsWith("\"")) {
-            return value.substring(1, value.length() - 1);
-        } else if ("true".equalsIgnoreCase(value)) {
-            return true;
-        } else if ("false".equalsIgnoreCase(value)) {
-            return false;
-        } else if (value.contains(".")) {
-            return Double.parseDouble(value);
-        } else {
-            return Integer.parseInt(value);
+    private static Object parseValue(IndexWapper iw) {
+        iw.skipSpaces();
+        Character chr = iw.charNow();
+        if (chr == '[') return parseList(iw);
+        if (chr == '{') return parseMap(iw);
+        return parsePrimitiveValue(iw);
+
+    }
+
+    private static ArrayList<Object> parseList(IndexWapper iw) {
+        ArrayList<Object> list = new ArrayList<>();
+        iw.next();
+        iw.skipSpaces();
+        while(iw.charNow() != ']'){
+            list.add(parseValue(iw));
         }
+        iw.next();
+        return list;
+    }
+
+    private static Object parsePrimitiveValue(IndexWapper iw) {
+
+        if (iw.charNow() == '"') {
+            int indexStart = iw.getIndex();
+            iw.next();
+            int indexEnd =  iw.nextWhileNot('"');
+            iw.nextWhileNotIn(",]}");
+            return iw.getData().substring(indexStart + 1, indexEnd).trim();
+        }
+
+        int indexSave = iw.getIndex();
+        iw.next();
+        iw.nextWhileNotIn(",]}");
+        String value = iw.getData().substring(indexSave, iw.getIndex()).trim();
+
+        if ("true".equals(value)) return true;
+        if ("false".equals(value)) return false;
+        if (value.contains(".")) return Double.parseDouble(value);
+        else return Integer.parseInt(value);
+
     }
 }
--- src/ru/vsu/sc/parser/Main.java
+++ src/ru/vsu/sc/parser/Main.java
@@ -2,21 +2,21 @@ package ru.vsu.sc.parser;
 
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        // чтение файла csv
-        String path = "C:\\Users\\Ideapad Gaming\\IdeaProjects\\Parser\\src\\ru\\vsu\\sc\\parser\\File\\data.csv";
-        String delimiter = ";";
-        CSVParser parser = new CSVParser(path, delimiter);
-        parser.parseCSV();
-        // чтение файла json
-        Map<String, Object> jsonMap = JSONParser.parseJSON("C:\\Users\\Ideapad Gaming\\IdeaProjects\\Parser\\src\\ru\\vsu\\sc\\parser\\File\\data1.json");
+        //// чтение файла csv
+        //String path = "C:\\Users\\Ideapad Gaming\\IdeaProjects\\Parser\\src\\ru\\vsu\\sc\\parser\\File\\data.csv";
+        //String delimiter = ";";
+        //CSVParser parser = new CSVParser(path, delimiter);
+        //parser.parseCSV();
+        //// чтение файла json
+        //Map<String, Object> jsonMap = (Map<String, Object>) JSONParser.parseJSON("src/ru/vsu/sc/parser/File/data1.json");
+        List<Object> jsonMap = (List<Object>) JSONParser.parseJSON("src/ru/vsu/sc/parser/File/data1.json");
 
         // Используем полученный объект jsonMap
         System.out.println(jsonMap);
-
-
     }
-    }
+}
